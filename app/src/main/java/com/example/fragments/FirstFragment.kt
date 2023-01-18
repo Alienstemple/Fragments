f83c7e509c881610217d59879fdc80d8c44a3cb7
Alyona <borisovsk_a@mail.ru>
2023-01-18 14:58:40 +0300

    feat: Add logs

--- app/src/main/java/com/example/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragments/FirstFragment.kt
@@ -1,6 +1,8 @@
 package com.example.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -25,6 +27,7 @@ class FirstFragment : Fragment() {
     lateinit var binding: FragmentFirstBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate() called with: savedInstanceState = $savedInstanceState")
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
@@ -36,9 +39,73 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+        Log.d(TAG,
+            "onCreateView() called with: inflater = $inflater, container = $container, savedInstanceState = $savedInstanceState")
         // Inflate the layout for this fragment
         binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        Log.d(TAG, "onAttach() called with: context = $context")
+        super.onAttach(context)
+        Log.d(TAG, "onAttach() called with: context = $context")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d(TAG,
+            "onViewCreated() called with: view = $view, savedInstanceState = $savedInstanceState")
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onActivityCreated() called with: savedInstanceState = $savedInstanceState")
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onViewStateRestored() called with: savedInstanceState = $savedInstanceState")
+        super.onViewStateRestored(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart() called")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume() called")
+        super.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG, "onSaveInstanceState() called with: outState = $outState")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause() called")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop() called")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.d(TAG, "onDestroyView() called")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy() called")
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        Log.d(TAG, "onDetach() called")
+        super.onDetach()
     }
 
     companion object {
@@ -54,5 +121,6 @@ class FirstFragment : Fragment() {
         @JvmStatic
         fun newInstance() =
             FirstFragment()
+        const val TAG = "FirstFragLog"
     }
 }
