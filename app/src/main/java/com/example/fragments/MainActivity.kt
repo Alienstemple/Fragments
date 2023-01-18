a1b4cc66fc2dc6a5561af2bbfac904419e7597ed
Alyona <borisovsk_a@mail.ru>
2023-01-18 14:19:47 +0300

    feat: Add fragment working

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import com.example.fragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Add new fragment
+        binding.addBtn.setOnClickListener {
+            if (savedInstanceState == null) {
+                val firstFragment = FirstFragment.newInstance()
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.test_frag_container, firstFragment)
+                    .commit()
+            }
+        }
+
+
     }
 }
