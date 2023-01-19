2caac046bbbdb24deb287bbb87c67380cb730ba6
Alyona <borisovsk_a@mail.ru>
2023-01-19 15:00:11 +0300

    feat: Add open process fragment

---
+++ app/src/main/java/com/example/fragments/OpenProcessFragment.kt
@@ -0,0 +1,80 @@
+package com.example.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.fragments.databinding.FragmentFirstBinding
+import com.example.fragments.databinding.FragmentOpenProcessBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+private var _binding: FragmentOpenProcessBinding? = null
+// This property is only valid between onCreateView and
+// onDestroyView.
+private val binding get() = _binding!!
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [OpenProcessFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class OpenProcessFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentOpenProcessBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding?.openProcessBtn?.setOnClickListener {
+            Toast.makeText(context, "Open pressed", Toast.LENGTH_SHORT).show()
+//            val transaction = parentFragmentManager.beginTransaction()
+//                .replace(R.id.dialog_frag_container, , "ENTER_NAME")
+//                .add(R.id.result_frag_container, )
+//            transaction.addToBackStack("ENTER_NAME")
+//            transaction.commit()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null  // Clear memory
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment OpenProcessFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+            OpenProcessFragment()
+    }
+}
--- app/src/main/java/com/example/fragments/SignInApp.kt
+++ app/src/main/java/com/example/fragments/SignInApp.kt
@@ -2,10 +2,25 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragments.databinding.ActivityMainBinding
+import com.example.fragments.databinding.ActivitySignInAppBinding
 
 class SignInApp : AppCompatActivity() {
+    lateinit var binding: ActivitySignInAppBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in_app)
+        binding = ActivitySignInAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        if (savedInstanceState == null) {
+            val openProcessFragment = OpenProcessFragment.newInstance()
+            val transaction = supportFragmentManager.beginTransaction()
+                .add(R.id.dialog_frag_container, openProcessFragment, "OPEN_PROCESS")
+            transaction.addToBackStack("OPEN_PROCESS")
+            transaction.commit()
+        }
+
     }
 }
