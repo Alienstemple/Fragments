08eacc34131700c7fdb8be627797ecdaf6d9475c
Alyona <borisovsk_a@mail.ru>
2023-01-23 14:33:05 +0300

    feat: SelectImage working

--- app/src/main/java/com/example/fragments/signfrags/ChooseImageFragment.kt
+++ app/src/main/java/com/example/fragments/signfrags/ChooseImageFragment.kt
@@ -6,11 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.RadioGroup
+import android.widget.RadioGroup.OnCheckedChangeListener
 import com.example.fragments.R
 import com.example.fragments.data.Account
 import com.example.fragments.databinding.FragmentChooseImageBinding
 import com.example.fragments.databinding.FragmentOpenProcessBinding
 import com.example.fragments.navigator
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class ChooseImageFragment : Fragment() {
     private var _binding: FragmentChooseImageBinding? = null
@@ -33,18 +37,41 @@ class ChooseImageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val bottomSheetDialog = BottomSheetDialog(requireContext())
+        val bottomSheetView = this.layoutInflater.inflate(R.layout.bottom_sheet_layout, null)
+        bottomSheetDialog.setContentView(bottomSheetView)
+
         binding.selectImageBtn.setOnClickListener {
             Log.d(TAG, "Btn img pressed.")
-            account = account.copy(img = 1)
-            Log.d(TAG, "In account : ${account.img}")
-//            navigator().publishResult(account)
-            Log.d(TAG, "Result published")
 
-            navigator().setImage(R.drawable.ic_launcher_foreground)
+            showDialogNotificationAction(bottomSheetDialog)
+            val radiogroup: RadioGroup = bottomSheetView.findViewById(R.id.img_choose_radiogroup)
+
+            radiogroup.setOnCheckedChangeListener { group, checkedId ->
+                val imgRes = when(checkedId) {
+                    R.id.img_radio_1 -> R.drawable.img1//R.drawable.ic_lock_idle_alarm
+                    R.id.img_radio_2 -> R.drawable.img2//R.drawable.ic_menu_copy_material
+                    else -> {throw RuntimeException("Except in radiogroup")}
+                }
+
+                navigator().setImage(imgRes)
+                bottomSheetDialog.dismiss()
+            }
+
         }
+
         binding.nextBtn.setOnClickListener {
             navigator() // TODO go to change account data
         }
+    }
+
+    private fun showDialogNotificationAction(bottomSheetDialog: BottomSheetDialog) {
+        bottomSheetDialog.show()
+
+        val bottomSheetDialogFrameLayout =
+            bottomSheetDialog.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)
+        bottomSheetDialogFrameLayout?.background = null
     }
 
     companion object {
